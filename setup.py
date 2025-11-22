f572c390aa547d76d9d7a948a5ed016a86d0de0a
khadar190798 <shaik.khadar1907@gmail.com>
2025-11-22 13:59:06 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project trains a simple machine-learning model (e.g., Linear Regression, RandomForest, or any classifier) and uses MLOps tools to manage:Data versioningPipeline automationExperiment trackingModel packaging & storageThis makes the workflow production-ready and scalable.',
+    author='Shaik Khadar Vali',
+    license='MIT',
+)
